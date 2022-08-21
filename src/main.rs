811b002c739b0976d78d0d765c5cd7dd73a0cf60
NazarK0 <staticman999@gmail.com>
2022-08-21 11:13:09 +0300

    complete game

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+mod draw;
+mod snake;
+mod game;
+
+extern crate rand;
+extern crate piston_window;
+extern crate sdl2_window;
+
+use piston_window::*;
+use sdl2_window::Sdl2Window;
+use piston_window::types::Color;
+use game::Game;
+use draw::to_coord_u32;
+
+const BLACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow<Sdl2Window> = WindowSettings::new(
+        "Snake",
+        [to_coord_u32(width), to_coord_u32(height)],
+    )
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        window.draw_2d(&event, |c, g, _| {
+            clear(BLACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
+
+}
